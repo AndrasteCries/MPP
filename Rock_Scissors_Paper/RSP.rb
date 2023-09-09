aef17f251a56d133c02ca1fbd3c0eb89bd1f2e90
Oleksandr Shynkarenko <1andraste1@gmail.com>
2023-09-09 21:50:19 +0300

    Complete RPS task

--- Rock_Scissors_Paper/RSP.rb
+++ Rock_Scissors_Paper/RSP.rb
@@ -1,1 +1,16 @@
-puts ("Hello world!")
+array = ["Scissors", "Paper", "Rock", "Scissors"]
+
+if ARGV[0] == ARGV[1]
+    puts("Draw")
+    exit
+end
+    
+for a in (0..array.length()) do
+   if ARGV[0] == array[a]
+       if ARGV[1] == array[a + 1]
+           puts("You win")
+           exit
+       end
+   end
+end
+puts("You lose")
