a0e9f4d8f1b740cfdc2ad04e70b000e4b21d7452
Oleksandr Shynkarenko <1andraste1@gmail.com>
2023-10-10 23:09:44 +0300

    fix 4, 9, 40 e.g

--- Roman_numerals/main.rb
+++ Roman_numerals/main.rb
@@ -1,4 +1,4 @@
-$roman_hash = {"M" => 1000, "D" => 500, "C" => 100, "L" => 50, "X" => 10, "V" => 5, "I" => 1}
+$roman_hash = {"M" => 1000, 'CM' => 900, "D" => 500, 'CD' => 400, "C" => 100, "XC" => 90, "L" => 50, "XL" => 40, "X" => 10, "IX" => 9, "V" => 5, "IV" => 4, "I" => 1}
 
 def to_roman(num)
   answer = ""
@@ -9,14 +9,14 @@ def to_roman(num)
     end
   end
   answer
-end 
+end
 
 def to_arabic(num)
   answer = 0
-  $roman_hash.each_pair do |roman, value|
-    while num[0] == roman
-      answer += value
-      num.slice!(0)
+  $roman_hash.keys.sort_by { |k| k.length }.reverse.each do |roman|
+    while num.start_with?(roman)
+      answer += $roman_hash[roman]
+      num = num[roman.length..-1]
     end
   end
   answer
